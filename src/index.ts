e8e18c6c359d35fc9427bf62f01657794667b8e0
Jhonatan.Bernardes <phantux894@gmail.com>
2025-01-16 16:45:00 -0300

    dist add

--- src/index.ts
+++ src/index.ts
@@ -14,24 +14,3 @@ const client = new ExtendedClient();
 
   export { client, config } 
 
-
-
-
-
-
-
-{
-//USA A FUNÇÃO DO FS PARA LER OS ARQUIVOS, UTILIZANDO O FOREACH PARA PERCORRER A PASTA E ENCONTRAR
-//ARQUIVOS FILTRADOS QUE CONTENHAM NO FINAL .TS OU .JS
-//
-/*fs.readdirSync(path.join(__dirname, "commands")).forEach(local => {
-
-  fs.readdirSync(path.join(__dirname, "commands", local))
-  .filter(fileName => fileName.endsWith(".ts") || fileName.endsWith(".js"))
-  .forEach(fileName => {
-    console.log(fileName)
-  })
-
-})
-*/
-}
