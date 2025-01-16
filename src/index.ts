eceae8f97449f498826a94911fb2e3cf5daac510
Jhonatan.Bernardes <phantux894@gmail.com>
2025-01-16 13:43:05 -0300

    bot bill

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+
+import { green } from "colors";
+import {ExtendedClient} from "./structs/ExtendedClient"
+export * from "colors"; 
+import config from "./config.json"
+import fs from "fs";
+import path from "path";
+
+
+
+const client = new ExtendedClient();
+           
+  client.start();         
+
+  export { client, config } 
+
+  
---
+++ src/structs/ExtendedClient.ts
@@ -0,0 +1,30 @@
+import { Client, Partials, IntentsBitField, BitFieldResolvable, GatewayIntentsString, Component, Collection} from "discord.js";
+import { CommandType, ComponentsButton, ComponentsSelect, ComponentsModal } from "./types/Command";
+
+import dotenv from "dotenv";
+
+dotenv.config(); 	
+
+export class ExtendedClient extends Client{
+    public commands: Collection<string, CommandType> = new Collection();
+    public buttons: ComponentsButton = new Collection();
+    public selects: ComponentsSelect = new Collection();
+    public modals: ComponentsModal = new Collection();
+        constructor(){
+            super({
+            intents: Object.keys(IntentsBitField.Flags) as BitFieldResolvable<GatewayIntentsString, number>,
+            partials: [
+            Partials.Channel, Partials.GuildMember, Partials.GuildScheduledEvent, Partials.Message
+            , Partials.Reaction, Partials.ThreadMember, Partials.User
+            ]
+        })
+    }
+    public async start(){
+        try{
+            this.login(process.env.BOT_TOKEN);
+            console.log('Bot está funcionando...');
+        } catch (error) {
+            console.error('Falha: ', error);
+        }
+    }
+}
