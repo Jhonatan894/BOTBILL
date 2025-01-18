841b3e51ed59b3ac1d33ae4f6cc3b0b44773982a
Guilhermin <guilhermehiato@hotmail.com>
2025-01-18 03:03:20 -0300

    Adicionei Arquivos na Main componentes e ready e editei tambem o ping.ts e o Extend.t

--- src/commands/common/ping.ts
+++ src/commands/common/ping.ts
@@ -1,11 +1,33 @@
-import { ApplicationCommandType } from "discord.js";
+import { ActionRowBuilder, ApplicationCommandType, ButtonBuilder, ButtonStyle, Collection } from "discord.js";
 import { Command } from "../../structs/types/Command";
 
 export default new Command({
-    name: "ping",
-    description: "receba o pong!",
-    type: ApplicationCommandType.ChatInput,
-    run({interaction}){
-        interaction.reply({ephemeral: true, content: "RECEBA O PONG BILL!!"})
-    }
-})
+  name: "ping",
+  description: "receba o pong!",
+  type: ApplicationCommandType.ChatInput,
+  run({ interaction }) {
+    const row = new ActionRowBuilder<ButtonBuilder>({
+      components: [
+        new ButtonBuilder({
+          customId: "test-button",
+          label: "Receba aqui",
+          style: ButtonStyle.Success,
+        }),
+      ],
+    });
+
+    interaction.reply({
+      ephemeral: true,
+      content: "RECEBA O PONG BILL!!",
+      components: [row],
+    });
+  },
+  buttons: new Collection([
+    [
+      "test-button",
+      (interaction) => {
+        interaction.update({ components: [] });
+      },
+    ],
+  ]),
+});
